de772c2ab2a2f1cbf8a6d18d154de6ce0913cd25
Glenn <glennc@mega7.fun>
2022-04-16 22:01:48 +0800

    skills

--- src/pages/skills/index.jsx
+++ src/pages/skills/index.jsx
@@ -3,8 +3,21 @@ import ParticlesComponent from '../../components/Particles'
 import styled from 'styled-components'
 import skillBackground from '../../images/skill-background.png'
 import { expnad } from '../../animation'
+import react from '../../images/logos/react.svg'
+import css from '../../images/logos/css.svg'
+import html from '../../images/logos/html.svg'
+import javascript from '../../images/logos/javascript.svg'
+import sass from '../../images/logos/sass.svg'
+import bootstrap from '../../images/logos/bootstrap.png'
+import next from '../../images/logos/next.png'
+import tailwind from '../../images/logos/tailwind.png'
+import typescript from '../../images/logos/typescript.png'
+import vue from '../../images/logos/vue.png'
+import redux from '../../images/logos/redux.png'
 
 const SkillSection = styled.section`
+	display: flex;
+	justify-content: center;
 	position: relative;
 	width: 2%;
 	height: 85%;
@@ -15,7 +28,14 @@ const SkillSection = styled.section`
 	background-size: cover;
 	animation: ${expnad} 1s forwards;
 	transform-origin: bottom;
-	transition: width 1s 1s;
+`
+
+const GridContainer = styled.div`
+	width: 800px;
+	display: grid;
+	grid-template-rows: repeat(3, 1fr);
+	grid-template-columns: repeat(3, 1fr);
+	grid-gap: 1rem;
 `
 
 const StyledBackground = styled.img`
@@ -26,45 +46,56 @@ const StyledBackground = styled.img`
 	left: 0;
 	opacity: 0.05;
 	object-fit: cover;
+	z-index: -1;
 `
 
-const LogoCard = styled.li`
-	width: 250px;
+const LogoCard = styled.div`
+	width: calc((800px - 2rem) / 3);
 	height: 300px;
-	background-color: white;
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
+	align-items: center;
 `
 
 const Logo = styled.img`
-	width: 100%;
+	width: 120px;
 	margin-bottom: 1rem;
 `
 
 const logoList = [
-	{ name: 'Html', id: 1 },
-	{ name: 'Css', id: 2 },
-	{ name: 'Javascript', id: 3 },
-	{ name: 'Typescript', id: 4 },
-	{ name: 'Sass', id: 5 },
-	{ name: 'Tailwind', id: 6 },
-	{ name: 'React', id: 7 },
-	{ name: 'Nextjs', id: 8 },
-	{ name: 'Redux/Toolkit', id: 9 },
-	{ name: 'Vue', id: 10 },
+	{ name: 'Html', id: 1, path: html },
+	{ name: 'Css', id: 2, path: css },
+	{ name: 'Javascript', id: 3, path: javascript },
+	{ name: 'Typescript', id: 4, path: typescript },
+	{ name: 'Sass', id: 5, path: sass },
+	{ name: 'Bootstrap', id: 6, path: bootstrap },
+	{ name: 'Tailwind', id: 7, path: tailwind },
+	{ name: 'React', id: 8, path: react },
+	{ name: 'Nextjs', id: 9, path: next },
+	{ name: 'Redux/Toolkit', id: 10, path: redux },
+	{ name: 'Vue', id: 11, path: vue },
 ]
 
 const Skills = () => {
-	// const renderLogoList = logoList.map((logo) => {
-	// 	return (
-	// 		<LogoCard>
-	// 			<Logo src={required('../../images/react')} />
-	// 		</LogoCard>
-	// 	)
-	// })
-
-	// console.log(require('../../../images/logos/react.svg'))
+	const renderLogoList = logoList.map((logo) => {
+		return (
+			<LogoCard key={logo.id}>
+				<div
+					style={{
+						width: 150,
+						height: 150,
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'center',
+					}}
+				>
+					<Logo src={logo.path} />
+				</div>
+				<p style={{ fontSize: '2rem' }}>{logo.name}</p>
+			</LogoCard>
+		)
+	})
 
 	return (
 		<>
@@ -72,9 +103,9 @@ const Skills = () => {
 			<ContentContainer>
 				<SkillSection>
 					<StyledBackground src={skillBackground} />
-					<LogoCard>
-						{/* <img src={require('../../../images/logos/react.svg')} alt="logo" /> */}
-					</LogoCard>
+					<div style={{ overflow: 'scroll', width: '100%', padding: '0 50px' }}>
+						<GridContainer>{renderLogoList}</GridContainer>
+					</div>
 				</SkillSection>
 			</ContentContainer>
 		</>
